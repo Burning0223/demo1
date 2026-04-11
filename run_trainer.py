dacc2c5f0928550f1d78c8c392638b26feb72230
Burning0223 <liuboning2004@126.com>
2026-04-11 13:10:59 +0800

    合并train.py和main.py(上一版未保存上），修改test.py

--- run_trainer.py
+++ run_trainer.py
@@ -1,24 +1,34 @@
 import swanlab
 from torch import nn
 import torch
-from untils import Metrics,EarlyStopping
+from untils import Metrics,EarlyStopping,Cls_Config
+import random
+import numpy as np
+from transformers import get_linear_schedule_with_warmup
+from data_process import TextClassificationDataset
+from model import BertClassifier
+from torch.utils.data import DataLoader
 
+def random_seed(seed):
+    random .seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
 
 
 class Trainer():
-    def __init__(self,model,optimizer,scheduler,patience,id2label,config):
+    def __init__(self,model,patience,id2label,max_length,num_epochs,batch_size,learning_rate,optimizer,scheduler):
         self.model=model
         self.optimizer=optimizer
         self.scheduler=scheduler
         self.early_stopping=EarlyStopping(patience=patience,verbose=True)
         self.id2label=id2label
-        self.max_length=config.get("max_length",128)
-        self.num_epochs=config.get("num_epochs",10)
-        self.batch_size=config.get("batch_size",16)
-        self.learning_rate=config.get("learning_rate",2e-5)
+        self.max_length=max_length
+        self.num_epochs=num_epochs
+        self.batch_size=batch_size
+        self.learning_rate=learning_rate
 
 
-        experiment_name=f"max_length:{self.max_length},num_epochs:{self.nfig.num_epochs},batch_size:{self.batch_size},learning_rate:{self.learning_rate}"
+        experiment_name=f"max_length:{self.max_length},num_epochs:{self.num_epochs},batch_size:{self.batch_size},learning_rate:{self.learning_rate}"
         swanlab.init(project="Bert_text_classification",
              experiment_name=experiment_name,
              config={
@@ -45,7 +55,7 @@ class Trainer():
             self.scheduler.step()
            
         ave_loss=total_loss/len(dataloader)
-        train_metrics=Metrics(labels,preds)
+        train_metrics=Metrics(labels,preds,self.id2label)
         train_acc=train_metrics.acc
         train_support=train_metrics.support
         return ave_loss,train_acc,train_support
@@ -62,7 +72,7 @@ class Trainer():
                 preds=torch.argmax(output,dim=1)
                 total_loss+=loss.item()
         ave_loss=total_loss/len(dataloader)
-        dev_metrics=Metrics(labels,preds)
+        dev_metrics=Metrics(labels,preds,self.id2label)
         dev_acc=dev_metrics.acc
         dev_report=dev_metrics.report
         return ave_loss,dev_acc,dev_report
@@ -80,10 +90,6 @@ class Trainer():
             print(f"验证损失:{dev_loss:.4f}")
             print(f"验证准确率:{dev_acc:.4f}")
             print(f"验证报告:\n{dev_report}")
-            """
-            :.4f 这种格式化写法只能用在 f-string 里
-            不能直接写在字典里
-            """
             
             swanlab.log({
                 "epoch":epoch,
@@ -103,3 +109,36 @@ class Trainer():
             
         print(f"模型最优准确率为：{dev_best_acc}")
     
+def main():
+    config=Cls_Config("Bert_Config.json")
+    data_path=config.get("data_path","../0.demo1文本分类/toutiao_cat_data.txt")
+    batch_size=config.get("batch_size",32)
+    max_length=config.get("max_length",128)
+    num_epochs=config.get("num_epochs",10)
+    batch_size=config.get("batch_size",16)
+    learning_rate=config.get("learning_rate",2e-5)
+    patience=config.get("patience",2)
+    random_seed(42)
+
+    train_dataset=TextClassificationDataset(data_path=data_path,dataset_type="train",config=config)
+    dev_dataset=TextClassificationDataset(data_path=data_path,dataset_type="dev",config=config)
+
+    id2label=train_dataset.id2label
+    num_classes=train_dataset.num_classes
+
+    train_dataloader=DataLoader(train_dataset,batch_size,shuffle=True,collate_fn=train_dataset.collate_fn)
+    dev_dataloader=DataLoader(dev_dataset,batch_size,shuffle=False,collate_fn=dev_dataset.collate_fn)
+   
+    model=BertClassifier(config,num_classes)
+
+    optimizer=torch.optim.AdamW(
+        model.parameters(),lr=learning_rate
+    )
+    total_steps = len(train_dataloader) * num_epochs
+    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=total_steps)
+    
+    trainer=Trainer(model,patience,id2label,max_length,num_epochs,batch_size,learning_rate,optimizer,scheduler)
+    trainer.train_with_early_stopping(train_dataloader,dev_dataloader)
+    
+if __name__=="__main__":
+    main()
--- test.py
+++ test.py
@@ -1,23 +1,28 @@
 import torch
 from model import BertClassifier
-from train import Trainer
-from Bert_Config import Cls_Config
+from run_trainer import Trainer
+from untils import Cls_Config
 from data_process import TextClassificationDataset
 from torch.utils.data import DataLoader
 def test():
     config=Cls_Config("Bert_Config.json")
     data_path=config.get("data_path","../0.demo1文本分类/toutiao_cat_data.txt")
     batch_size=config.get("batch_size",32)
+    max_length=config.get("max_length",128)
+    num_epochs=config.get("num_epochs",10)
+    batch_size=config.get("batch_size",16)
+    learning_rate=config.get("learning_rate",2e-5)
     patience=config.get("patience",2)
 
     test_dataset=TextClassificationDataset(data_path=data_path,dataset_type="test",config=config)
     test_dataloader=DataLoader(test_dataset,batch_size,shuffle=False,collate_fn=test_dataset.collate_fn)
     num_classes=test_dataset.num_classes
+    id2label=test_dataset.id2label
     model=BertClassifier(config,num_classes)
     checkpoint=torch.load("checkpoint.pt") #手动填写
     model.load_state_dict(checkpoint['model'])
 
-    trainer=Trainer(model,optimizer=None,scheduler=None,patience=patience)
+    trainer=Trainer(model,patience,id2label,max_length,num_epochs,batch_size,learning_rate,optimizer=None,schedule=None)
     test_loss,test_acc,test_report=trainer.dev(test_dataloader)
     print(f"测试损失: {test_loss:.4f}")
     print(f"测试准确率: {test_acc:.4f}")
