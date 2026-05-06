db128d8dde7a7b28473a5d855ca54831ed19d80f
Burning0223 <liuboning2004@126.com>
2026-05-06 11:08:23 +0800

    重新加载数据集

--- data_process.py
+++ data_process.py
@@ -8,16 +8,8 @@ class TextClassificationDataset(Dataset):
     def __init__(self,config,dataset_type):
         super().__init__()
         self.config=config
-        if dataset_type=="train":
-            csv_file=self.config.train_data_path
-        elif dataset_type=="dev":
-            csv_file=self.config.dev_data_path
-        elif dataset_type=="test":
-            csv_file=self.config.test_data_path
-        else:
-            raise ValueError(f"Unknown dataset type: {dataset_type}")
         self.label2id,self.id2label=self.get_id_label()
-        self.texts,self.keywords,self.labels=self.load_data(csv_file)
+        self.texts,self.keywords,self.labels=self.load_data(dataset_type)
         self.tokenizer=BertTokenizer.from_pretrained(config.model_path)
         
     def get_id_label(self):
@@ -38,13 +30,38 @@ class TextClassificationDataset(Dataset):
                 json.dump(mappings,f,ensure_ascii=False,indent=4)
 
         return label2id,id2label
-    def load_data(self,csv_file):
-        data=pd.read_csv(csv_file)
-        texts=data['text'].tolist()
-        keywords=data['keyword'].tolist()
-        labels=data['label'].tolist()
-        labels=[self.label2id.get(label,-1) for label in labels]
-        return texts,keywords,labels
+    def load_data(self,dataset_type):
+        if dataset_type == "train":
+            file_path = f"{self.config.data_path}/train_3k.txt"
+        elif dataset_type == "dev":
+            file_path = f"{self.config.data_path}/dev_1k.txt"
+        elif dataset_type == "test":
+            file_path = f"{self.config.data_path}/test_1k.txt"
+        else:
+            raise ValueError(f"Unknown dataset type: {dataset_type}")
+        texts=[]
+        labels=[]
+        keywords=[]
+        with open(file_path,'r',encoding='utf-8') as f:
+            for line in f:
+                parts=line.strip().split("_!_")
+
+                label_code=int(parts[1])
+                text=parts[3]
+                keyword=parts[4]
+
+                keyword=keyword.replace(","," ")
+                texts.append(text)
+                keywords.append(keyword)
+
+                if label_code in self.label2id:
+                    label=self.label2id[label_code]
+                else:
+                    print(f"警告：标签{label_code}未在label2id中找到,默认为-1")
+                    label=-1
+                labels.append(label)
+
+        return texts,keywords,labels 
 
     def __len__(self):
         return len(self.labels)
